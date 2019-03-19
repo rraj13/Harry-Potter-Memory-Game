dcb56553407d87339ae8d98d5885ff2cfc8eb949
Rahul Raj <rahulrajshopping@icloud.com>
2019-03-18 23:21:52 -0400

    Updates

---
+++ precache-manifest.38e0792f26b41b6f0d68591cbdfea41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "601a38d572aa3b3445720a5ea09ba88c",
+    "url": "/Harry-Potter-Memory-Game/static/media/hogwarts.601a38d5.jpg"
+  },
+  {
+    "revision": "8bbff49e1ca1d7481e6b",
+    "url": "/Harry-Potter-Memory-Game/static/js/runtime~main.847046bd.js"
+  },
+  {
+    "revision": "660971d02f30eb574d61",
+    "url": "/Harry-Potter-Memory-Game/static/js/main.ce7e0bc0.chunk.js"
+  },
+  {
+    "revision": "8dff5ae49e8ef34df594",
+    "url": "/Harry-Potter-Memory-Game/static/js/2.efcc78c1.chunk.js"
+  },
+  {
+    "revision": "660971d02f30eb574d61",
+    "url": "/Harry-Potter-Memory-Game/static/css/main.43248fdc.chunk.css"
+  },
+  {
+    "revision": "50ef480dd0c251fc8dc627204163a864",
+    "url": "/Harry-Potter-Memory-Game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Harry-Potter-Memory-Game/precache-manifest.38e0792f26b41b6f0d68591cbdfea41e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Harry-Potter-Memory-Game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
